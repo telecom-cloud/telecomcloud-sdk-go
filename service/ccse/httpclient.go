89bc29d9fd898027631b3dab4551ea864c0d221d
xusai <xusai@chinatelecom.cn>
2026-04-14 17:43:19 +0800

    feat(httpclient): add dy tob sign

--- service/ccse/httpclient.go
+++ service/ccse/httpclient.go
@@ -17,6 +17,8 @@ package ccse
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
@@ -435,7 +437,22 @@ func (r *request) SetRequestOption(option ...config.RequestOption) *request {
 func (r *request) Execute(method, url string) (*response, error) {
 	r.method = method
 	r.url = url
+	r.addTobSign()
 	return r.client.Execute(r)
+}
+
+func (r *request) addTobSign() {
+	info := r.header.Get("dy-tob-accountInfo")
+	if info != "" {
+		r.queryParam.Set("dyTobSign", MD5([]byte(info)))
+	}
+}
+
+func MD5(data []byte) string {
+	hash := md5.New()
+	hash.Write(data)
+	hashBytes := hash.Sum(nil)
+	return hex.EncodeToString(hashBytes)
 }
 
 func parseRequestURL(c *HttpClient, r *request) error {
